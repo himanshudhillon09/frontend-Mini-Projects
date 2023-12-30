c2ac13383e162693488f004dff68d75787b53822
Himanshu Dhillon <himanshu.dhillon09@gmail.com>
2023-12-31 01:52:08 +0530

    Completed Profile Share Modal Project

---
+++ Profile Share Modal/index.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector(".modal-container");
+const overlay = document.querySelector(".overlay");
+
+
+const openModal = () => {
+    console.log("Modal is Open");
+    modal.classList.add("active");
+    overlay.classList.add("overlayActive");
+}
+
+
+
+
+const closeModal = () => {
+    modal.classList.remove("active");
+    overlay.classList.remove("overlayActive");
+}
